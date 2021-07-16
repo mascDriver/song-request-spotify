310c1304eae27050984606fe6eeb8948f3494f81
mascdriver <diogoblibi@hotmail.com>
2021-07-16 20:41:42 -0300

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,20 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
     ],
   },
   optimization: {
