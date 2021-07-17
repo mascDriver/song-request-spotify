68bcf4d1419e70b61ce1a31fd4089b3e9bcbdc0d
mascdriver <diogoblibi@hotmail.com>
2021-07-16 21:45:49 -0300

    make it better

--- song_request/settings.py
+++ song_request/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'song_request.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db9ldrs54a016f',
+        'USER': 'kzcvlriaotixlo',
+        'PASSWORD': 'f2704430e6d0c55a1081ed395a0122ae0907c953008ce6956f89955ff212b87a',
+        'HOST': 'ec2-34-193-101-0.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
