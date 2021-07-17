777af50e5048f4e4589afde8c46f570cb0297a60
mascdriver <diogoblibi@hotmail.com>
2021-07-16 21:04:35 -0300

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,32 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./frontend/src/index.js",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "./frontend/static/frontend"),
+    path: path.resolve(__dirname, "./static/frontend"),
     filename: "[name].js",
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      }
     ],
   },
   optimization: {
     minimize: true,
   },
   plugins: [
-        new HtmlWebPackPlugin({
-            template: "./frontend/templates/frontend/index.html",
-            filename: "./index.html"
-        })
-    ]
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
 };
