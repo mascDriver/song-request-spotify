93073d2be6286d6985a7923242ca734c946f549b
mascdriver <diogoblibi@hotmail.com>
2021-07-17 15:22:44 -0300

    make it better

--- spotify/models.py
+++ spotify/models.py
@@ -5,10 +5,10 @@ from api.models import Room
 class SpotifyToken(models.Model):
     user = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    refresh_token = models.CharField(max_length=150)
-    access_token = models.CharField(max_length=150)
+    refresh_token = models.CharField(max_length=200)
+    access_token = models.CharField(max_length=200)
     expires_in = models.DateTimeField()
-    token_type = models.CharField(max_length=50)
+    token_type = models.CharField(max_length=200)
 
 
 class Vote(models.Model):
