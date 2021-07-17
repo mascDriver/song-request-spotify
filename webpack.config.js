f971fb1ccdbf9eb0ef7304a4d602088b54d9c5de
mascdriver <diogoblibi@hotmail.com>
2021-07-16 21:07:31 -0300

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./frontend/src/index.js",
   output: {
-    path: path.resolve(__dirname, "./static/frontend"),
+    path: path.resolve(__dirname, "./frontend/static/frontend"),
     filename: "[name].js",
   },
   module: {
