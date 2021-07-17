a0d9e757abee219fd16ba42d3bdd907153729ee0
mascdriver <diogoblibi@hotmail.com>
2021-07-17 14:59:06 -0300

    make it better

--- song_request/settings.py
+++ song_request/settings.py
@@ -135,8 +135,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
